54c52999ae7b349714f024ffb7a19b3a323c4e08
laiyefei <laiyefei0710@163.com>
2020-06-25 16:03:04 +0800

    []

---
+++ src/main/java/com/study/algorithnm/AlgorithnmApplication.java
@@ -0,0 +1,13 @@
+package com.study.algorithnm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithnmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithnmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/algorithnm/AlgorithnmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.algorithnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithnmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
